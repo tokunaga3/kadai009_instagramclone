389433f778bad016e7b075edf619596799240ed6
mutsukitokunaga <mutsukitokunaga@outlook.jp>
2020-12-24 08:02:12 +0000

    課題提出04

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,6 @@ Rails.application.routes.draw do
   end
   resources :sessions, only: [:new, :create, :destroy]
   resources :users
-  resources :blogs
   resources :favorites, only: [:create, :destroy, :index]
-  mount LetterOpenerWeb::Engine, at: "/letter_opener" 
+  # mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
 end

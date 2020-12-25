beec218492d2b83ae7ac718ad2adb955b66c07f6
mutsukitokunaga <mutsukitokunaga@outlook.jp>
2020-12-25 10:25:23 +0000

    ログイン機能調整

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,7 +8,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      redirect_to user_path(@user.id)
+      redirect_to new_session_path(@user.id)
     else
       render :new
     end
@@ -26,7 +26,7 @@ class UsersController < ApplicationController
   end
 
   def edit
-    if @user == current_user.id
+    if @user.id == current_user.id
     else
       redirect_to feeds_path, notice: "ログインユーザーが違うので編集できません"
     end
